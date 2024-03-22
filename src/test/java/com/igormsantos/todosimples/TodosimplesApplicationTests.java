4f9125f8e99ac6538608efc25c76246686f23224
Igor M. Santos <igormsprofissional@gmail.com>
2024-03-22 20:04:09 -0300

    first commit

---
+++ src/test/java/com/igormsantos/todosimples/TodosimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igormsantos.todosimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
